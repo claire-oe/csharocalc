a1ecb2d037feaa4f364c5cc6e5b4b7505f27b217
Claire Egorp <63665834+claire-oe@users.noreply.github.com>
2022-01-22 15:43:46 +0000

    Added additional features to calculator

--- StringCalculator_Add.cs
+++ StringCalculator_Add.cs
@@ -23,7 +23,6 @@ namespace StringCal
 		  var result=_calculator.Add(numbers);
 		  Assert.Equal(expectedResult,result);
 		}
-		}
 	/*Method to return sum of 1 or 2 numbers */	
 		[Theory]
 		[InlineData("1,2",3)]
@@ -39,6 +38,45 @@ namespace StringCal
 		[InlineData("1,2,3",6)]
 		[InlineData("2,3,4",9)]
 	  public void ReturnsSumGivenStringWithThreeCommaSeparatedNumbers(string numbers,int expectedResult)
+		{
+		  var result=_calculator.Add(numbers);
+		  Assert.Equal(expectedResult,result);
+		} 
+	 /* Allow the Add method to handle new lines between numbers (in addition to commas).*/
+		[Theory]
+		[InlineData("1\n2,3",6)]
+		[InlineData("1\n2\n3",6)]
+		[InlineData("1,2\n3",6)]
+	  public void ReturnsSumGivenStringWithThreeCommaOrNewlineSeparatedNumbers(string numbers,int expectedResult)
+		{
+		  var result=_calculator.Add(numbers);
+		  Assert.Equal(expectedResult,result);
+		}
+ 	/* Support different delimiters */
+ 		[Theory]
+		[InlineData("//;\n1;2;3",6)]
+	  public void ReturnsSumGivenStringWithCustomDelimiter(string numbers,int expectedResult)
+		{
+		  var result=_calculator.Add(numbers);
+		  Assert.Equal(expectedResult,result);
+		}  
+	/*Calling Add for a negative number will throw an exception "negatives not allowed" - and the negative */
+	/*that was passed, if there are multiple negatives, show all of them in the exception message*/
+		[Theory]
+		[InlineData("-1,2","Negatives not allowed:-1")]
+		[InlineData("-1,-2","Negatives not allowed:-1,-2")]
+	  public void ThrowsGivenNegativeInputs(string numbers,string expectedMessage)
+		{
+		  Action action=()=>_calculator.Add(numbers);
+		  var ex=Assert.Throws<Exception>(action);
+		  Assert.Equal(expectedMessage,ex.Message);
+		}  
+	/* Numbers bigger than 1000 would be ignored, so adding 2 + 1001 = 2 */
+		[Theory]
+		[InlineData("1,2,3000",3)]
+		[InlineData("1001,2",2)]
+		[InlineData("1000,2",1002)]
+	  public void ReturnsSumGivenStringIgnoringValuesOver1000(string numbers,int expectedResult)
 		{
 		  var result=_calculator.Add(numbers);
 		  Assert.Equal(expectedResult,result);
